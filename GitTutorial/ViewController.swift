1b2b1c0f0448cd1f216054bbd3651dd295ea75d6
waltersiegrist <walter.siegrist@gmail.com>
2018-03-31 17:04:24 +0200

    another change

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello CS193p")
+        print("hello hello CS193p")
         print("hello world!")
         // Do any additional setup after loading the view, typically from a nib.
     }
